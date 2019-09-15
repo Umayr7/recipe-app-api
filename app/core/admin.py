01803d4aa255ed39dc97c6b156be475a0f978cff
Umayr7 <umairnadeem.baloch@gmail.com>
2019-09-16 01:43:04 +0500

    Updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,27 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),  # section1
+        (_('Personal Info'), {'fields': ('name',)}),  # section2
+        (_('Permissions'), {'fields': ('is_active',
+                                       'is_staff', 'is_superuser')}),  # section3
+        (_('Important Dates'), {'fields': ('last_login',)})  # section4
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
